cc64ded9b946704a9e10ced3c706cc856bd2ac92
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-09-04 19:33:52 -0300

    feat: criacao do app module

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import mikroOrmConfig from '../mikro-orm.config.js';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MikroOrmModule.forRoot(mikroOrmConfig),
+  ],
+})
+export class AppModule {}
